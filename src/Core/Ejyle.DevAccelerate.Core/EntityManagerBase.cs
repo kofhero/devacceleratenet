66b15ee8e074304eea0eedd40273ac6ef9168709
Tanveer Yousuf <tanveery@outlook.in>
2018-06-18 17:20:12 +0530

    Removed Entity Framework NuGet package from the Core project.

--- src/Core/Ejyle.DevAccelerate.Core/EntityManagerBase.cs
+++ src/Core/Ejyle.DevAccelerate.Core/EntityManagerBase.cs
@@ -4,7 +4,6 @@
 // ----------------------------------------------------------------------------------------------------------------------
 
 using System;
-using System.Data.Entity;
 
 namespace Ejyle.DevAccelerate.Core
 {
