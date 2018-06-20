c0ace73701034a984621985ac84601a8d21dfaeb
Tanveer Yousuf <tanveery@outlook.in>
2018-06-20 12:21:01 +0530

    Added core unit test project.

---
+++ src/Core/Ejyle.DevAccelerate.Core.UnitTests/MailProviderUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Mail;
+using Ejyle.DevAccelerate.Core.Mail;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ejyle.DevAccelerate.Core.UnitTests
+{
+    [TestClass]
+    public class MailProviderUnitTests
+    {
+        [TestMethod]
+        public void GetMailProviderTest()
+        {
+            var mailProvider = MailProviderFactory.GetProvider();
+            Assert.IsNotNull(mailProvider, "Mail provider is null.");
+        }
+
+        [TestMethod]
+        public void SendMailMessage()
+        {
+            var mailProvider = MailProviderFactory.GetProvider();
+            mailProvider.Send(new MailMessage("someone@example.com", "someone@example.com")
+            {
+                Subject = "Some subject",
+                Body = "Some message"
+            });
+        }
+    }
+}
---
+++ src/Core/Ejyle.DevAccelerate.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ejyle.DevAccelerate.Core.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ejyle.DevAccelerate.Core.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b25fc243-fce1-4554-a1d3-83d82a02ff32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
